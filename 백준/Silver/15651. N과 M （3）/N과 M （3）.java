73a84fa9bac407be48e4f88ff0090219d8d9d70e
jihyelee <110345050+C0Zl@users.noreply.github.com>
2024-03-21 14:37:02 +0900

    [Silver III] Title: N과 M (3), Time: 464 ms, Memory: 68420 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static int N, M;
+	public static int[] arr;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] s = br.readLine().split(" ");
+		
+		N = Integer.parseInt(s[0]);
+		M = Integer.parseInt(s[1]);
+		
+		arr = new int[M];
+		
+		comb(0);
+		System.out.println(sb);
+	}
+	
+	private static void comb(int depth) {
+		if (depth == M) {
+			for (int i : arr) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			
+			arr[depth] = i;
+			comb(depth + 1);
+			
+		}
+	}
+}
