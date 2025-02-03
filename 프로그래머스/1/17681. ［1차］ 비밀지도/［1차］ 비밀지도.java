632e64fa17eab50081f72bd65122e83eff047820
jihyelee <110345050+C0Zl@users.noreply.github.com>
2025-02-03 19:59:54 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,47 +1,19 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        boolean[][] wall = new boolean[n][n];
         String[] answer = new String[n];
-        for(int i = 0; i < n; i++) {
-            answer[i] = "";
-        }
-
-        // 각 10진수 숫자들을 순회
-        for (int i = 0; i < n; i++) {
-            int num = arr1[i];
-            
-            // 10진수 숫자들을 2진수로 계산
-            for(int j = n - 1; j >= 0; j--) {
-                if (num % 2 == 1) {
-                    wall[i][j] = true;
-                }
-                num /= 2;
-            }
-        }
-        
-        // 각 10진수 숫자들을 순회
-        for (int i = 0; i < n; i++) {
-            int num = arr2[i];
-            
-            // 10진수 숫자들을 2진수로 계산
-            for(int j = n - 1; j >= 0; j--) {
-                if (num % 2 == 1) {
-                    wall[i][j] = true;
-                }
-                num /= 2;
-            }
-        }
         
         for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            int num = arr1[i] | arr2[i];
+            
             for (int j = 0; j < n; j++) {
-                if(wall[i][j]) answer[i] += "#";
-                else answer[i] += " ";
+                sb.insert(0, (num % 2 == 1 ? "#" : " "));
+                num /= 2;
             }
-        }
-        
+            
+            answer[i] = sb.toString();
+        }        
         
         return answer;
     }
-    
-    // public void 
 }
