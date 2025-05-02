d5f1e77b9a24327daf1598d1375e3d581ddb34eb
jihyelee <110345050+C0Zl@users.noreply.github.com>
2025-05-03 00:48:09 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/README.md
+++ 프로그래머스/3/59042. 없어진 기록 찾기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 없어진 기록 찾기 - 59042 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59042) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59042?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 04월 24일 23:19:15
+2025년 05월 03일 00:48:07
 
 ### 문제 설명
 
--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,14 +1,7 @@
 -- 코드를 입력하세요
-SELECT
-    ao.animal_id AS ANIMAL_ID,
-    ao.name AS NAME
-FROM
-    animal_outs ao
-LEFT OUTER JOIN
-    animal_ins ai
-ON
-    ao.animal_id = ai.animal_id
-WHERE
-    ai.animal_id IS NULL
-ORDER BY
-    ANIMAL_ID
+SELECT AO.ANIMAL_ID AS ANIMAL_ID, AO.NAME AS NAME
+FROM ANIMAL_OUTS AO
+LEFT JOIN ANIMAL_INS AI
+ON AO.ANIMAL_ID = AI.ANIMAL_ID
+WHERE AI.ANIMAL_ID IS NULL
+ORDER BY ANIMAL_ID ASC;
