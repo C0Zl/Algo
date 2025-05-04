a664a18551c3b6b302334f78efd9f46f8c899042
jihyelee <110345050+C0Zl@users.noreply.github.com>
2025-05-04 11:45:54 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 0.41 ms
+메모리: 87.6 MB, 시간: 0.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 05일 09:32:01
+2025년 05월 04일 11:45:50
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,33 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] visited = new boolean[n];
+    int[][] computers;
+    boolean[] visited;
 
-        for(int i = 0; i < n; i++) {
-            if (!visited[i]) { // 방문하지 않은 네트워크 찾기
-                bfs(i, n, computers, visited);
-                answer++; // 새로운 네트워크 발견 시 증가
+    public int solution(int n, int[][] computers) {
+        int answer = 0;        
+        this.computers = computers;
+        this.visited = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                answer++;
             }
         }
+        
         return answer;
     }
-
-    private void bfs(int start, int n, int[][] computers, boolean[] visited) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        visited[start] = true;
-
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-
-            for(int j = 0; j < n; j++) { // 모든 노드 검사
-                if (computers[node][j] == 1 && !visited[j]) {
-                    queue.add(j);
-                    visited[j] = true;
-                }
+    
+    public void dfs(int i) {
+        visited[i] = true;
+        
+        for (int j = 0; j < computers.length; j++) {
+            if (i != j && computers[i][j] == 1 && !visited[j]) {
+                dfs(j);
             }
         }
+        
+        return;
     }
 }
