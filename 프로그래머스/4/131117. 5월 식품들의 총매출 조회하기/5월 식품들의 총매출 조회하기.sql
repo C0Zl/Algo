e93e3e20785f37548100fb9bc41edda01540079d
jihyelee <110345050+C0Zl@users.noreply.github.com>
2025-05-06 19:45:33 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,16 +1,17 @@
 SELECT
-    p.product_id AS PRODUCT_ID,
-    p.product_name AS PRODUCT_NAME,
-    p.price * SUM(o.amount) AS TOTAL_SALES
+    P.PRODUCT_ID,
+    PRODUCT_NAME,
+    PRICE * SUM(AMOUNT) AS TOTAL_SALES
 FROM
-    food_product p
-LEFT JOIN
-    food_order o
+    FOOD_PRODUCT P
+JOIN
+    FOOD_ORDER O
 ON
-    p.product_id = o.product_id
+    P.PRODUCT_ID = O.PRODUCT_ID
 WHERE
-    DATE(o.produce_date) BETWEEN '2022-05-01' AND '2022-05-31'
+    PRODUCE_DATE >= TO_DATE('2022-05-01', 'YYYY-MM-DD')
+    AND PRODUCE_DATE < TO_DATE('2022-06-01', 'YYYY-MM-DD')
 GROUP BY
-    p.product_id
+    P.PRODUCT_ID, PRODUCT_NAME, PRICE
 ORDER BY
     TOTAL_SALES DESC, PRODUCT_ID ASC
