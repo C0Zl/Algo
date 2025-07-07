acea4fdcb1f056290a38b4e41d3e2b7c40228b21
jihyelee <110345050+C0Zl@users.noreply.github.com>
2025-07-07 17:04:20 +0900

    [Silver IV] Title: 설탕 배달, Time: 168 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        // N 입력
+        int N = sc.nextInt();
+        int n = N;
+        int minCnt = n / 5;
+        n %= 5;
+
+        while (n > 0) {
+            // 현재 3으로 나누어 떨어지는지 확인
+            if (n % 3 == 0) {
+                minCnt += n / 3;
+                System.out.println(minCnt);
+                return;
+            } else {
+                n += 5;
+                minCnt--;
+
+                if (n > N) {
+                    System.out.println(-1);
+                    return;
+                }
+            }
+        }
+
+        System.out.println(minCnt);
+    }
+}
